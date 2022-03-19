f8937a2dcfad0e58e7d26e4a2c8d5d7d19aff100
Kota Morishita <44553544+kotamorishi@users.noreply.github.com>
2022-03-19 18:00:11 -0400

    Install script for root user

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt -y install libopenjp2-7 libtiff5 libatlas-base-dev
+sudo -H pip3 install Pillow
+sudo -H pip3 install numpy
+sudo -H pip3 install matplotlib
+sudo -H pip3 install gpiod
+sudo -H pip3 install schedule
