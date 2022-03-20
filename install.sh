2726baa1640ae9164a74f7ed0748df42c4ea9b40
Kota Morishita <44553544+kotamorishi@users.noreply.github.com>
2022-03-19 20:18:01 -0400

    clone this repo

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-sudo apt -y install python3-pip libopenjp2-7 libtiff5 libatlas-base-dev
+sudo apt -y git install python3-pip libopenjp2-7 libtiff5 libatlas-base-dev
 sudo -H pip3 install Pillow
 sudo -H pip3 install numpy
 sudo -H pip3 install matplotlib
 sudo -H pip3 install gpiod
 sudo -H pip3 install schedule
+git clone https://github.com/kotamorishi/weather-impression
